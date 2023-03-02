f4cc9c5fef43835d4c4d198671ea6d3a876db65f
shruthiga-kalyan <114078474+shruthiga-kalyan@users.noreply.github.com>
2023-03-02 14:56:34 +0530

    Delete Book directory

--- Book/src/main/java/com/example/demo/Repository/BookRepos.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Entity.BookDetails;
-
-public interface BookRepos extends JpaRepository<BookDetails,Integer>{
-
-}
