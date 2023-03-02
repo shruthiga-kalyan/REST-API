56bff94d1781b39f20194251a89b52eeacf75aba
shruthiga-kalyan <114078474+shruthiga-kalyan@users.noreply.github.com>
2023-03-02 14:56:47 +0530

    Delete Children directory

--- Children/src/main/java/com/example/demo/Repository/ChildrenRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Entity.Children;
-
-public interface ChildrenRepo extends JpaRepository<Children,Integer> {
-
-}
